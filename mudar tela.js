ee9b842f64e69704cfe7d9ff71798eed0d18956f
gustavojosecruz-source <gustavo.jose.cruz@escola.pr.gov.br>
2026-09-17 11:34:27 -0300

    Add files via upload

---
+++ mudar tela.js
@@ -0,0 +1,8 @@
+function mudaTema(){
+    const corpoPagina = document.body;
+    if(corpoPagina.classList.contains("tema-escuro")){
+        corpoPagina.classList.remove("tema-escuro");
+    } else{
+        corpoPagina.classList.add("tema-escuro");
+    }
+}
